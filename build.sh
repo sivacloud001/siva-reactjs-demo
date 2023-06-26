9b77b3965cf17c57453824cb558dc76d900e995c
sivanand <126378270+sivacloud001@users.noreply.github.com>
2023-06-26 23:59:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
-#!bin/bash
+#!/bin/bash
 
+# Build the Docker image
+sudo docker build -t react-js .
 
-docker build .
+# Tag the Docker image with the Docker Hub registry and image name
+sudo docker tag react-js sivacloud001/dev:react-app1
 
+# Push the Docker image to Docker Hub
+sudo docker push sivacloud001/dev:react-app1
