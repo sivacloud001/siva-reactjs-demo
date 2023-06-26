3de5b6ee4946e0cb4f8ec716ad1ae4ae06c46818
sivanand <126378270+sivacloud001@users.noreply.github.com>
2023-06-26 23:59:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
-#!bin/bash
+#!/bin/bash
+
+# Stop and remove the existing container
+## docker stop react-app1
+## docker rm react-app1
+
+# Pull the latest image from Docker Hub
+docker pull sivacloud001/dev:react-app1
+
 # Start a new container with the updated image
-docker run -itd -p 80:80 127512bebea9
-
-
+docker run --rm -itd --network host -p 3000:80  --name mykk sivacloud001/dev:react-app1
